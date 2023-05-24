dd27089d37c8afba10ff8fdca7e5695fb24bb2d3
Gabin ARNONE <gabin@arnone.org>
2023-05-24 12:09:43 +0200

    Push

--- expo.php
+++ expo.php
@@ -1,11 +1,16 @@
 <?php require_once('functions.php'); ?>
-<style> body{
-            background-image: url('images/petite-galerie-5-figure-d-artiste-2.webp');}</style>
         
 <?php require_once('_header.php'); ?>
     <?php require_once('_nav.php'); ?>
 <h1>Bienvenue dans notre exposition</h1>
 <H2>Catégories: </H2>
+<figure>
+    <img class="tableaux" src="tableaux\800px-Mona_Lisa__by_Leonardo_da_Vinci__from_C2RMF_retouched.jpg" alt="">
+    <figcaption>
+        <a href="Tableaux.php">Tableaux</a>
+    </figcaption>
+</figure>
+
 </body>
 <footer>
     <address>
