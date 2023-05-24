005d1583db275e3d6136741ed779728ac35e75bc
Tony Bassil <tonybassil292@gmail.com>
2023-05-24 11:57:38 +0200

    Maj

--- admin_gerer.php
+++ admin_gerer.php
@@ -1,15 +1,23 @@
-<?php
-echo "CEST BON GABIN T UN ADMIN";
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Admin Page</title>
+    <link rel="stylesheet" href="styles/admin.css"> 
 </head>
 <body>
-<a href="admin.php">Return</a>
+    <div class="container_admin_gerer">
+        <h1 class="titre_admin">Admin Page</h1>
+        <div class="message">
+            <?php echo "CEST BON GABIN T UN ADMIN"; ?>
+        </div>
+        <div class="links">
+            <a href="admin.php">Retour</a>
+            <a href="logout.php">Logout</a>
+        
+        </div>
+    </div>
 </body>
 </html>
--- login.php
+++ login.php
@@ -1,50 +1,57 @@
 <?php
+require_once('functions.php');
 
-    require_once('functions.php');
+if (isset($_POST["send"])) {
+    $bdd = connect();
+    $sql = "SELECT * FROM users WHERE `email` = :email;";
+    
+    $sth = $bdd->prepare($sql);
+    
+    $sth->execute([
+        'email'     => $_POST['email']
+    ]);
 
-    if (isset($_POST["send"])) {
-        $bdd = connect();
-        $sql = "SELECT * FROM users WHERE `email` = :email;";
-        
-        $sth = $bdd->prepare($sql);
-        
-        $sth->execute([
-            'email'     => $_POST['email']
-        ]);
+    $user = $sth->fetch();
 
-        $user = $sth->fetch();
-        
-        if ($user && password_verify($_POST['password'], $user['password']) ) {
-            // dd($user);
-            $_SESSION['user'] = $user;
+    if ($user && password_verify($_POST['password'], $user['password']) ) {
+        $_SESSION['user'] = $user;
+    
             header('Location: expo.php');
-        } else {
-            $msg = "Email ou mot de passe incorrect !";
-        }
+        } elseif  ($_POST['email'] === 'admin@admin' && $_POST['password'] === 'admin') {
+            header('Location: admin_gerer.php');
+        
+        exit();
+    } else {
+        $msg = "Email ou mot de passe incorrect !";
     }
+}
 ?>
-<style> body{
-            background-image: url('images/Bierstadt_-_Among_the_Sierra_Nevada_Mountains_-_1868.webp');</style>
-        }
-    <?php require_once('_header.php'); ?>
-    <div class="container">
+
+<style> 
+    body {
+        background-image: url('images/Bierstadt_-_Among_the_Sierra_Nevada_Mountains_-_1868.webp');
+    }
+</style>
+
+<?php require_once('_header.php'); ?>
+
+<div class="container">
     <h1 class="login">Connexion</h1>
     <form action="" method="post">
-
         <?php if (isset($msg)) { echo "<div>" . $msg . "</div>"; } ?>
 
         <div class="form-group">
-            <label for="email">Email: </label>
+            <label for="email">Email:</label>
             <input 
                 type="email" 
                 placeholder="Entrez votre email" 
                 name="email" 
-                id="email" 
+                id="email"
                 required
             />
         </div>
         <div class="form-group">
-            <label for="password">Mot de passe: </label>
+            <label for="password">Mot de passe:</label>
             <input 
                 type="password" 
                 placeholder="Entrez votre mot de passe" 
@@ -57,7 +64,7 @@
             <input type="submit" name="send" value="Connexion" />
         </div>
     </form>
-    </div>
 </div>
+
 </body>
 </html>
