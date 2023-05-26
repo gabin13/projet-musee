fb23b2fb8cfab734428f7cc42d2a75e46f1cccce
Gabin ARNONE <gabin@arnone.org>
2023-05-26 02:33:35 +0200

    style

--- expo.php
+++ expo.php
@@ -11,18 +11,24 @@
     Partagez cette expérience avec vos proches. 
     Explorez, apprenez et émerveillez-vous devant notre musée virtuel. Bonne visite !</p><br>
 <H2>Catégories: </H2>
-<figure>
-    <img class="tableaux" src="tableaux\800px-Mona_Lisa__by_Leonardo_da_Vinci__from_C2RMF_retouched.jpg" alt="">
-    <figcaption>
-        <a href="Tableaux.php">Tableaux</a>
-    </figcaption>
-</figure>
-<figure>
-    <img class="sculptures" src="sculptures\Le-Penseur-rodin-575x1024.png" alt="">
-    <figcaption>
-        <a href="sculptures.php">Sculptures</a>
-    </figcaption>
-</figure>
+
+<a href="tableaux.php">
+    <figure>
+        <img class="tableaux" src="tableaux\800px-Mona_Lisa__by_Leonardo_da_Vinci__from_C2RMF_retouched.jpg" alt="">
+        <figcaption>
+            <a href="tableaux.php">Tableaux</a>
+        </figcaption>
+    </figure>
+</a>
+
+<a href="sculptures.php">
+    <figure>
+        <img class="sculptures" src="sculptures\Le-Penseur-rodin-575x1024.png" alt="">
+        <figcaption>
+            <a href="sculptures.php">Sculptures</a>
+        </figcaption>
+    </figure>
+</a>
 
 </body>
 </html>
