f28d5c72138b1772c9d7c6cc5e67fcca790f9fbf
Aurélien <90250923+Aure0310@users.noreply.github.com>
2023-05-27 10:36:35 +0200

    Update achat.php

--- achat.php
+++ achat.php
@@ -1,40 +1,36 @@
 <link rel="stylesheet" href="styles/achat.css">
-<?php require_once('functions.php'); ?>
+<?php require_once('functions.php'); 
+
+$bdd = connect();
+
+$sql = "SELECT * FROM boutique ";
+$sth = $bdd->prepare($sql);
+$sth->execute();
+$boutique = $sth->fetchAll();
+
+?> 
 
 <br><br><br>
+
 <div class="container">
   <?php $productID = $_GET['id'];
 
-if ($productID == 1) { ?>
-    <div class="product">
-      <img src="Images/tshirt.jpg" alt="T-Shirt Joconde">
-    </div>
-    <div class="product-info">
-      <h2 class="product-title">T-Shirt Joconde</h2>
-      <p class="product-description">Un T-Shirt incontournable.</p>
-      <p class="product-price">29.99€</p>
-    </div>
-  <?php } elseif ($productID == 2) { ?>
-    <div class="product">
-      <img src="Images/tableaucène.jpg" alt="Tableau Cène">
-    </div>
-    <div class="product-info">
-      <h2 class="product-title">Tableau Cène</h2>
-      <p class="product-description">Un tableau représentant une célèbre peinture du musée, la Cène.</p>
-      <p class="product-price">19.99€</p>
-    </div>
-  <?php } elseif ($productID == 3) { ?>
-    <div class="product">
-      <img src="Images/mug.jpg" alt="Mug David">
-    </div>
-    <div class="product-info">
-      <h2 class="product-title">Mug David</h2>
-      <p class="product-description">Un mug représentant la célèbre sculpture de Michel-Ange, David.</p>
-      <p class="product-price">14.99€</p>
-    </div>
-  <?php } else {
-    echo 'Produit non trouvé.';
-  } ?>
+ 
+if ($boutique) {
+  foreach ($boutique as $produit) {
+    $nom = $produit['nom'];
+    $description = $produit['description'];
+    $image = $produit['image'];
+    $prix = $produit['prix'];
+
+    $id = $produit['id'];
+
+    if ($id == $_GET['id']) {
+      echo "<div>Produit : $nom</div>";
+    }
+  }
+}
+?>
 
   <form action="traitement.php" method="post">
     <label for="nom">Nom :</label>
