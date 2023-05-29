570b2ba4e458e439f3bc6e5b0da7b065dde28488
Aurélien <90250923+Aure0310@users.noreply.github.com>
2023-05-29 14:53:11 +0200

    Update avatar.php

--- avatar.php
+++ avatar.php
@@ -78,8 +78,23 @@
     <div class="avatar-option">
       <img src="Images/avatar5.jpg" alt="Avatar 5" onclick="selectAvatar(5)">
     </div>
+    </div>
+    <br>
+    <div class="avatar-list">
     <div class="avatar-option">
       <img src="Images/avatar6.jpg" alt="Avatar 6" onclick="selectAvatar(6)">
+    </div>
+    <div class="avatar-option">
+      <img src="Images/avatar7.jpg" alt="Avatar 7" onclick="selectAvatar(7)">
+    </div>
+    <div class="avatar-option">
+      <img src="Images/avatar8.jpg" alt="Avatar 8" onclick="selectAvatar(8)">
+    </div>
+    <div class="avatar-option">
+      <img src="Images/avatar9.jpg" alt="Avatar 9" onclick="selectAvatar(9)">
+    </div>
+    <div class="avatar-option">
+      <img src="Images/avatar10.jpg" alt="Avatar 10" onclick="selectAvatar(10)">
     </div>
   </div>
 
